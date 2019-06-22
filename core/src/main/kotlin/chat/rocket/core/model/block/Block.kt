9cb6e5b7b5edd3f8c774d096ad6f2bb5769e93c1
Cool-fire <upendrareddy2511@gmail.com>
2019-06-22 09:53:11 +0530

    Modify Blocks Support

--- core/src/main/kotlin/chat/rocket/core/model/block/Block.kt
+++ core/src/main/kotlin/chat/rocket/core/model/block/Block.kt
@@ -1,6 +1,11 @@
 package chat.rocket.core.model.block
+import chat.rocket.core.model.block.elements.Element
+import chat.rocket.core.model.block.objects.TextObject
 
-
-interface Block {
-    var type : String
-}
+data class Block(
+        var type: String? = null,
+        var text: TextObject? = null,
+        var blockId: String? = null,
+        var accessory : Element? = null,
+        var elements: List<Element>? = null
+)
