214703e32c1bec57fccd3f7df54bba57eff28bf8
Cool-fire <upendrareddy2511@gmail.com>
2019-07-23 10:48:43 +0530

    add json serialization

--- core/src/main/kotlin/chat/rocket/core/internal/model/elementPayload/DatePickerElementPayload.kt
+++ core/src/main/kotlin/chat/rocket/core/internal/model/elementPayload/DatePickerElementPayload.kt
@@ -1,7 +1,9 @@
 package chat.rocket.core.internal.model.elementPayload
 
 import com.squareup.moshi.Json
+import se.ansman.kotshi.JsonSerializable
 
+@JsonSerializable
 data class DatePickerElementPayload(
         override val type: String,
         override val blockId: String?,
